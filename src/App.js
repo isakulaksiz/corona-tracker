23a0d764a0d197f9332e3b5e1194f94a402b7362
isakulaksiz <isakulaksiz@outlook.com>
2020-09-29 20:08:13 +0300

    Add responsive design(mobile)

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import {Cards, Chart, CountryPicker } from './components';
 import styles from './App.module.css';
 import { fetchData } from './api';
 
+import coronaImage from './images/image.png';
 
  class App extends React.Component{
     state = {
@@ -33,6 +34,7 @@ import { fetchData } from './api';
          const { data, country } = this.state;
          return(
              <div className={styles.container}>
+                 <img className={styles.image} src={coronaImage} alt="Covid-19"/>
                  <Cards data={data}/>
                  <CountryPicker handleCountryChange={this.handleCountryChange} />
                  <Chart data={data} country={country} />
